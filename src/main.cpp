e8daff4e02870c09a6d113e24cfeac9dbc43f3d2
simo-vargasp-sonarsource <121958530+simo-vargasp-sonarsource@users.noreply.github.com>
2023-07-25 15:21:47 +0200

    Revert "Update main.cpp"

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,6 @@
 
 using namespace std;
 
-
-int array[10];
-array[10] = 0; // Noncompliant: index should be between 0 & 9
-
-char *buffer1 = (char *) malloc(100);
-char *buffer2 = (char *) malloc(50);
-memcpy(buffer2, buffer1, 100); // Noncompliant: buffer2 will overflow.
-
-
-
 int main(int argc, char* argv[]) {
   int num = argc - 1;
 
@@ -23,7 +13,8 @@ int main(int argc, char* argv[]) {
     cout << "2 arguments provided\n";
   } else {
     cout << num << " arguments provided\n";
-  } if (argv != 0) {
+  }
+  if (argv != 0) {
     cout << "argv not null\n";; // intentional extra-semicolon
   }
   if (argv == nullptr) {
@@ -31,12 +22,5 @@ int main(int argc, char* argv[]) {
   }
 
   return 0;
-}
-
-if (num == 0 ) {
-   cout << "2 arguments provided\n";
-
-} if (num 2) {  
- cout << "2 arguments provided\n";
 }
 
